351d446f1083047e77998e07d4515a449ad521f2
paulo andre andre <advpaulolima1@gmail.com>
2026-04-08 16:14:05 -0300

    fix: ajuste do path base para o deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pje-calc-pro/', // Adicione exatamente o nome do seu repositório aqui entre barras
 })
