8dcce0aac0ef4739ee67205a3ee041f8ea97aa79
Paulo <advpaulolima1@gmail.com>
2026-04-13 21:16:31 -0300

    correção do arquivo exemplos pdf

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pje-calc-pro/', // Adicione esta linha com o nome do seu repositório no GitHub
+  base: '/pje-calc-pro/', 
 })
